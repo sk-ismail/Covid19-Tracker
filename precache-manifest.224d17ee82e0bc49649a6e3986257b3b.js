439ec2f5e1d9d7f4be8fc6a442015b02891c9505
irfan738 <skismail23@yahoo.com>
2020-09-24 03:02:25 +0530

    Updates

---
+++ precache-manifest.224d17ee82e0bc49649a6e3986257b3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9de01e32f79ca0b448629ee391255f7",
+    "url": "/Covid19-Tracker/index.html"
+  },
+  {
+    "revision": "053049a072eaf0fe248c",
+    "url": "/Covid19-Tracker/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "1facb65000df6790e065",
+    "url": "/Covid19-Tracker/static/css/main.d0a33b68.chunk.css"
+  },
+  {
+    "revision": "053049a072eaf0fe248c",
+    "url": "/Covid19-Tracker/static/js/2.fb97ecb8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Covid19-Tracker/static/js/2.fb97ecb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1facb65000df6790e065",
+    "url": "/Covid19-Tracker/static/js/main.776af306.chunk.js"
+  },
+  {
+    "revision": "58dd2541b5ff91463868",
+    "url": "/Covid19-Tracker/static/js/runtime-main.b658d57a.js"
+  }
+]);
